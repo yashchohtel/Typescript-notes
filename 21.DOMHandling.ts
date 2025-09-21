6861ad1e043c18656dd16ff403f6b9c4b050d468
Yash Chohtel <yashchohtel@gmail.com>
2025-09-21 22:56:09 +0530

    dom odne

---
+++ 21.DOMHandling.ts
@@ -0,0 +1,6 @@
+/*
+
+DOM Handling - If you’ve done DOM handling in JavaScript, it’s almost the same — the difference is: TypeScript adds type safety so you don’t accidentally call .value on something that isn’t an input.z
+
+*/ 
+  
