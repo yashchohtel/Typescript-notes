f3cf5d31d7bf78f39a5c0700921f304b6ff60455
Yash Chohtel <yashchohtel@gmail.com>
2025-09-22 23:38:41 +0530

    accessModified started

--- 23.AccessModifiers.ts
+++ 23.AccessModifiers.ts
@@ -1,7 +1,15 @@
 /*
 
+✅ 1. What are Access Modifiers?
+
+Access modifiers control visibility of class members (properties and methods).
+They determine where you can access a property or method:
+
+Inside the class only
+
+Inside class and subclasses
+
+Anywhere
+
 ----------------------------------------------
-
-✅
-
 */ 
